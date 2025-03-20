8df0a3613c3a74f0b477fe2fb714dea098ba4b59
Silal <111568366+Silal123@users.noreply.github.com>
2025-03-20 21:17:24 +0100

    addmore features and better formatting

--- informer.py
+++ informer.py
@@ -1,102 +1,146 @@
 import aiohttp
 import aiohttp.web
-import json as Json
-import os
-import time
-import sys
 import psutil
 import platform
 
-import config
+import json as Json
+import os
 
-routes = aiohttp.web.RouteTableDef()
+#! Config
 
-@routes.get('/')
-async def main(request: aiohttp.web.Request):
-    return aiohttp.web.json_response({'status': 'success', 'message': 'server is online and api ready'}, status=200)
+class Config():
+    def __init__(self):
+        self.json = {}
+        self.init()
+        self.load()
+        self.dump()
+        pass
 
-@routes.get('/sys/info/all')
-async def all_sys_info(request: aiohttp.web.Request):
-    auth_header = str(request.headers.get('Authorization'))
-    if not auth_header:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
-    
-    if not auth_header in config.json['tokens']:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+    def init(self):
+        if not os.path.exists('config.json'): 
+            with open('config.json', 'a') as json_file:
+                Json.dump({}, json_file, indent=4)
+                json_file.close()
 
-    ram_byte = psutil.virtual_memory().total
-    used_ram_byte = psutil.virtual_memory().used
-    return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
-        'ram': {
-            'total': {'b': ram_byte, 'kb': round(ram_byte/1024, 1), 'mb': round(ram_byte/1048576, 1), 'gb': round(ram_byte/1073741824, 1)}, 
-            'used': {'b': used_ram_byte, 'kb': round(used_ram_byte/1024, 1), 'mb': round(used_ram_byte/1048576, 1), 'gb': round(used_ram_byte/1073741824, 1), 'percent': round(100/ram_byte*used_ram_byte, 1)}
-        },
-        'cpu': {
-            'freq': psutil.cpu_freq(), 'count': psutil.cpu_count(), 'percent': psutil.cpu_percent()
-        },
-        'os': {
-            'name': platform.system().lower(), 'release': platform.release(), 'version': platform.version(), 'architecture': platform.architecture()
-        }
-    }}, status=200)
+    def load(self):
+        with open('config.json', 'r') as json_file:
+            self.json = Json.load(json_file)
 
-@routes.get('/sys/info/small')
-async def small_sys_info(request: aiohttp.web.Request):
-    auth_header = str(request.headers.get('Authorization'))
-    if not auth_header:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
-    
-    if not auth_header in config.json['tokens']:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+        if not self.json.get("tokens"): self.json["tokens"] = []
 
-    return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
-        'ram': {'total': psutil.virtual_memory().total, 'used': psutil.virtual_memory().used},
-        'cpu': {'percent': psutil.cpu_percent()},
-        'os': {'name': platform.system().lower(), 'release': platform.release()}
-    }}, status=200)
+    def dump(self):
+        with open('config.json', 'w') as json_file:
+            Json.dump(self.json, json_file, indent=4)
 
-@routes.get('/sys/info/ram')
-async def ram_sys_info(request: aiohttp.web.Request):
-    auth_header = str(request.headers.get('Authorization'))
-    if not auth_header:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
-    
-    if not auth_header in config.json['tokens']:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
-    
-    ram_byte = psutil.virtual_memory().total
-    used_ram_byte = psutil.virtual_memory().used
-    return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
-        'total': {'b': ram_byte, 'kb': round(ram_byte/1024, 1), 'mb': round(ram_byte/1048576, 1), 'gb': round(ram_byte/1073741824, 1)}, 
-        'used': {'b': used_ram_byte, 'kb': round(used_ram_byte/1024, 1), 'mb': round(used_ram_byte/1048576, 1), 'gb': round(used_ram_byte/1073741824, 1), 'percent': round(100/ram_byte*used_ram_byte, 1)}
-        }}, status=200)
+config = Config()
 
-@routes.get('/sys/info/cpu')
-async def cpu_sys_info(request: aiohttp.web.Request):
-    auth_header = str(request.headers.get('Authorization'))
-    if not auth_header:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
-    
-    if not auth_header in config.json['tokens']:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
-    
-    return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
-        'freq': psutil.cpu_freq(), 'count': psutil.cpu_count(), 'percent': psutil.cpu_percent()
-    }}, status=200)
+#! Server
 
-@routes.get('/sys/info/os')
-async def os_sys_info(request: aiohttp.web.Request):
-    auth_header = str(request.headers.get('Authorization'))
-    if not auth_header:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
-    
-    if not auth_header in config.json['tokens']:
-        return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+class Server():
+    def __init__(self):
+        self.routes = aiohttp.web.RouteTableDef()
 
-    return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
-        'name': platform.system().lower(), 'release': platform.release(), 'version': platform.version(), 'architecture': platform.architecture()
-    }}, status=200)
+        @self.routes.get('/')
+        async def main(request: aiohttp.web.Request):
+            return aiohttp.web.json_response({'status': 'success', 'message': 'server is online and api ready'}, status=200)
 
-app = aiohttp.web.Application()
-app.add_routes(routes)
+        @self.routes.get('/sys/info/all')
+        async def all_sys_info(request: aiohttp.web.Request):
+            auth_header = str(request.headers.get('Authorization'))
+            if not auth_header:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
+            
+            if not auth_header in config.json['tokens']:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
 
-aiohttp.web.run_app(app, port=8080)
+            ram_byte = psutil.virtual_memory().total
+            used_ram_byte = psutil.virtual_memory().used
+            return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
+                'ram': {
+                    'total': {'b': ram_byte, 'kb': round(ram_byte/1024, 1), 'mb': round(ram_byte/1048576, 1), 'gb': round(ram_byte/1073741824, 1)}, 
+                    'used': {'b': used_ram_byte, 'kb': round(used_ram_byte/1024, 1), 'mb': round(used_ram_byte/1048576, 1), 'gb': round(used_ram_byte/1073741824, 1), 'percent': round(100/ram_byte*used_ram_byte, 1)}
+                },
+                'cpu': {
+                    'freq': psutil.cpu_freq(), 'count': psutil.cpu_count(), 'percent': psutil.cpu_percent()
+                },
+                'os': {
+                    'name': platform.system().lower(), 'release': platform.release(), 'version': platform.version(), 'architecture': platform.architecture()
+                }
+            }}, status=200)
+
+        @self.routes.get('/sys/info/small')
+        async def small_sys_info(request: aiohttp.web.Request):
+            auth_header = str(request.headers.get('Authorization'))
+            if not auth_header:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
+            
+            if not auth_header in config.json['tokens']:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+
+            return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
+                'ram': {'total': psutil.virtual_memory().total, 'used': psutil.virtual_memory().used},
+                'cpu': {'percent': psutil.cpu_percent()},
+                'os': {'name': platform.system().lower(), 'release': platform.release()}
+            }}, status=200)
+
+        @self.routes.get('/sys/info/ram')
+        async def ram_sys_info(request: aiohttp.web.Request):
+            auth_header = str(request.headers.get('Authorization'))
+            if not auth_header:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
+            
+            if not auth_header in config.json['tokens']:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+            
+            ram_byte = psutil.virtual_memory().total
+            used_ram_byte = psutil.virtual_memory().used
+            return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
+                'total': {'b': ram_byte, 'kb': round(ram_byte/1024, 1), 'mb': round(ram_byte/1048576, 1), 'gb': round(ram_byte/1073741824, 1)}, 
+                'used': {'b': used_ram_byte, 'kb': round(used_ram_byte/1024, 1), 'mb': round(used_ram_byte/1048576, 1), 'gb': round(used_ram_byte/1073741824, 1), 'percent': round(100/ram_byte*used_ram_byte, 1)}
+                }}, status=200)
+
+        @self.routes.get('/sys/info/cpu')
+        async def cpu_sys_info(request: aiohttp.web.Request):
+            auth_header = str(request.headers.get('Authorization'))
+            if not auth_header:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
+            
+            if not auth_header in config.json['tokens']:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+            
+            return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
+                'freq': psutil.cpu_freq(), 'count': psutil.cpu_count(), 'percent': psutil.cpu_percent()
+            }}, status=200)
+
+        @self.routes.get('/sys/info/os')
+        async def os_sys_info(request: aiohttp.web.Request):
+            auth_header = str(request.headers.get('Authorization'))
+            if not auth_header:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'The token is misseng in the request!'}, status=401)
+            
+            if not auth_header in config.json['tokens']:
+                return aiohttp.web.json_response({'status': 'error', 'message': 'Your token has no access to this ressourche!'}, status=401)
+
+            return aiohttp.web.json_response({'status': 'success', 'message': 'data was called', 'data': {
+                'name': platform.system().lower(), 'release': platform.release(), 'version': platform.version(), 'architecture': platform.architecture()
+            }}, status=200)
+        
+    def run(self, port = 8080, host = '0.0.0.0'):
+        self.app = aiohttp.web.Application()
+        self.app.add_routes(self.routes)
+        aiohttp.web.run_app(self.app, port=port, host=host)
+
+server = Server()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="System info server to call info from your system")
+    parser.add_argument('--host', default='0.0.0.0', help="Server host (default: 0.0.0.0)")
+    parser.add_argument('--port', type=int, default=8080, help="Port to use (default: 8080)")
+
+    args = parser.parse_args()
+
+    print(f"Starting server on {args.host}:{args.port}")
+    print(f"Tokens: {config.json['tokens']}")
+    server.run(port=args.port, host=args.host)
